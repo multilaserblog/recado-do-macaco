21ad1990a41c982d3473248a77d85f46dbc86e28
multilaserblog <multilaserbis@gmail.com>
2024-12-03 17:07:58 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+document.getElementById("som-1").addEventListener("click", function(){
+    console.log("Botão 1 clicado"); 
+    document.getElementById("audio-1").play()
+
+});
+
+document.getElementById("som-2").addEventListener("click", function(){
+    console.log("Botão 2 clicando");
+    document.getElementById("audio-2").play()
+});
+
+document.getElementById("som-3").addEventListener("click", function(){
+    console.log("Botão 3 clicando");
+    document.getElementById("audio-3").play()
+});
+document.getElementById("som-4").addEventListener("click", function(){
+    console.log("Botão 4 clicando");
+    document.getElementById("audio-4").play()
+});
+
+document.getElementById("som-5").addEventListener("click", function(){
+    console.log("Botão 5 clicando");
+    document.getElementById("audio-5").play()
+});
+
